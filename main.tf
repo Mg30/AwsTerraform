dcbf71e39e861ff05b12712d8a7c063383e4e80c
Matt <matgonzalez736@gmail.com>
2020-04-01 14:48:57 +0200

    final version emr cluster

--- main.tf
+++ main.tf
@@ -4,27 +4,229 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.10.0.0/16"
-  }
-
-module "public_network" {
-  vpc_cidr = "10.10.0.0/16"
-  source = "./modules/public_network"
-  vpc_id = aws_vpc.my_vpc.id
-  public_subnet_newbits = 4
-  public_subnet_netnum = 2
-  filepath_public_SSH_key = "./ssh_key.pub"
-  file_private_SSH_key = "./ssh_key"
-  instances_list = {
-    i1= {
-    instance_type = "t2.micro"
-    ami = "ami-0fc61db8544a617ed"
-  },
-  i2 = {
-    instance_type = "t2.micro"
-    ami = "ami-0fc61db8544a617ed"
-  }}
+  enable_dns_hostnames = true
 }
 
-output "all_public_ip" {
-  value = module.public_network.all_public_ip
+module "public_network" {
+  vpc_cidr              = "10.10.0.0/16"
+  source                = "./modules/public_network"
+  vpc_id                = aws_vpc.my_vpc.id
+  public_subnet_newbits = 4
+  public_subnet_netnum  = 2
+}
+
+
+
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh_inbound"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_key_pair" "emr_key" {
+  key_name   = "emr_key"
+  public_key = file("access/id_rsa.pub")
+}
+
+resource "aws_emr_cluster" "my_cluster" {
+  name                              = "emr_big_data"
+  release_label                     = "emr-5.29.0"
+  applications                      = ["Spark", "Hadoop", "Hive", "Pig"]
+  termination_protection            = false
+  keep_job_flow_alive_when_no_steps = true
+  ec2_attributes {
+    key_name                          = aws_key_pair.emr_key.key_name
+    subnet_id                         = module.public_network.public_subnet_id
+    additional_master_security_groups = aws_security_group.ssh_sg.id
+    additional_slave_security_groups  = aws_security_group.ssh_sg.id
+    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+  }
+  master_instance_group {
+    instance_type = "m5.xlarge"
+  }
+  core_instance_group {
+    instance_type  = "m5.xlarge"
+    instance_count = 2
+    ebs_config {
+      size                 = "64"
+      type                 = "gp2"
+      volumes_per_instance = 1
+    }
+  }
+  service_role = aws_iam_role.iam_emr_service_role.arn
+
+}
+
+
+resource "aws_iam_role" "iam_emr_service_role" {
+  name = "iam_emr_service_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticmapreduce.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "iam_emr_service_policy" {
+  name = "iam_emr_service_policy"
+  role = aws_iam_role.iam_emr_service_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Resource": "*",
+        "Action": [
+            "ec2:AuthorizeSecurityGroupEgress",
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:CancelSpotInstanceRequests",
+            "ec2:CreateNetworkInterface",
+            "ec2:CreateSecurityGroup",
+            "ec2:CreateTags",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DeleteSecurityGroup",
+            "ec2:DeleteTags",
+            "ec2:DescribeAvailabilityZones",
+            "ec2:DescribeAccountAttributes",
+            "ec2:DescribeDhcpOptions",
+            "ec2:DescribeInstanceStatus",
+            "ec2:DescribeInstances",
+            "ec2:DescribeKeyPairs",
+            "ec2:DescribeNetworkAcls",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DescribePrefixLists",
+            "ec2:DescribeRouteTables",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeSpotInstanceRequests",
+            "ec2:DescribeSpotPriceHistory",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeVpcAttribute",
+            "ec2:DescribeVpcEndpoints",
+            "ec2:DescribeVpcEndpointServices",
+            "ec2:DescribeVpcs",
+            "ec2:DetachNetworkInterface",
+            "ec2:ModifyImageAttribute",
+            "ec2:ModifyInstanceAttribute",
+            "ec2:RequestSpotInstances",
+            "ec2:RevokeSecurityGroupEgress",
+            "ec2:RunInstances",
+            "ec2:TerminateInstances",
+            "ec2:DeleteVolume",
+            "ec2:DescribeVolumeStatus",
+            "ec2:DescribeVolumes",
+            "ec2:DetachVolume",
+            "iam:GetRole",
+            "iam:GetRolePolicy",
+            "iam:ListInstanceProfiles",
+            "iam:ListRolePolicies",
+            "iam:PassRole",
+            "s3:CreateBucket",
+            "s3:Get*",
+            "s3:List*",
+            "sdb:BatchPutAttributes",
+            "sdb:Select",
+            "sqs:CreateQueue",
+            "sqs:Delete*",
+            "sqs:GetQueue*",
+            "sqs:PurgeQueue",
+            "sqs:ReceiveMessage"
+        ]
+    }]
+}
+EOF
+}
+
+# IAM Role for EC2 Instance Profile
+resource "aws_iam_role" "iam_emr_profile_role" {
+  name = "iam_emr_profile_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "emr_profile" {
+  name  = "emr_profile"
+  roles = ["${aws_iam_role.iam_emr_profile_role.name}"]
+}
+
+resource "aws_iam_role_policy" "iam_emr_profile_policy" {
+  name = "iam_emr_profile_policy"
+  role = aws_iam_role.iam_emr_profile_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Resource": "*",
+        "Action": [
+            "cloudwatch:*",
+            "dynamodb:*",
+            "ec2:Describe*",
+            "elasticmapreduce:Describe*",
+            "elasticmapreduce:ListBootstrapActions",
+            "elasticmapreduce:ListClusters",
+            "elasticmapreduce:ListInstanceGroups",
+            "elasticmapreduce:ListInstances",
+            "elasticmapreduce:ListSteps",
+            "kinesis:CreateStream",
+            "kinesis:DeleteStream",
+            "kinesis:DescribeStream",
+            "kinesis:GetRecords",
+            "kinesis:GetShardIterator",
+            "kinesis:MergeShards",
+            "kinesis:PutRecord",
+            "kinesis:SplitShard",
+            "rds:Describe*",
+            "s3:*",
+            "sdb:*",
+            "sns:*",
+            "sqs:*"
+        ]
+    }]
+}
+EOF
+}
+
+
+output "master_dns" {
+  value = aws_emr_cluster.my_cluster.master_public_dns
 }
